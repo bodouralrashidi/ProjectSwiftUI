4f0a0636e8dce3f3f9819d66f5e17c4fe87fce43
Rawan <https://github.com/wawooona/SwiftUI.git>
2020-05-18 07:05:26 +0300

    added the sign in

--- ProjectSwiftUI/TABS/EmotionState.swift
+++ ProjectSwiftUI/TABS/EmotionState.swift
@@ -11,3 +11,9 @@ class Env : ObservableObject{
     @Published var variable : emotions = .happy
     
 }
+
+struct EmotionState_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ ProjectSwiftUI/TABS/SignInView.swift
@@ -0,0 +1,21 @@
+//
+//  SignInView.swift
+//  ProjectSwiftUI
+//
+//  Created by Rawan Alhammadi on 5/18/20.
+//  Copyright © 2020 Bodour Alrashidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
